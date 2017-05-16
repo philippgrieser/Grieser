36069efa50d7df41784bb23f8d4969595b4d90c5
philippgrieser <philippgrieser@googlemail.com>
2017-05-16 11:04:15 +0200

    clean up

---
+++ app/components/contact-form.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  captchaSolved: false,
+  buttonDisabled: false,
+  valueName: '',
+  valueMail: '',
+  valueNumber: '',
+  valueMessage: '',
+
+  actions: {
+    send(){
+      this.set('buttonDisabled', true);
+      emailjs.send("default_service", "webseite_template", {
+        name: this.get('valueName'),
+        message: this.get('valueMessage'),
+        number: this.get('valueNumber'),
+        mail: this.get('valueMail')
+      });
+    },
+    onCaptchaResolved(reCaptchaResponse) {
+      if(reCaptchaResponse) {
+        this.set('captchaSolved', true);
+      }
+    },
+  }
+});
