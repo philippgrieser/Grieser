095d68d6cdfd1aea34d887e0988015d674a0d1f7
Philipp <grieser@newsenselab.com>
2018-09-05 00:42:47 +0200

    rss feed

--- app/components/rss-feed.js
+++ app/components/rss-feed.js
@@ -2,4 +2,34 @@ import Ember from "ember";
 import moment from "moment";
 
 export default Ember.Component.extend({
+	didInsertElement() {
+		const url = 'https://www.datev.de/web/de/rss/nachrichten-steuern.rss';
+		const query = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from feednormalizer where url="' + url + '"') + '&format=json';
+
+		// send request
+		Ember.$.getJSON(query, function (data, status, errorThrown) {
+			if (status === 'success') {
+				$.each(data.query.results.rss.channel.item, function (key, value) {
+					console.log(value);
+
+					const entry = '<div class="entry">' +
+						'<h2><a href=\"' + value.link + '\">' + moment(value.pubDate).format("DD.MM.YYYY") + ": " + value.title + '</a></h2>' +
+						'<p>' + value.description + '</p>' +
+						'</div>';
+
+					// * append entire entry in container
+					$('#feed-container').append(entry);
+				});
+
+			} else if (status === 'error' || status === 'parsererror') {
+				// * log error message in console
+				console.log(errorThrown);
+
+				// * show error message
+				alert('Could not load RSS feed!');
+			}
+		});
+	},
+
+
 });
